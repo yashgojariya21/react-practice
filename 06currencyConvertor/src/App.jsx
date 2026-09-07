b2d9f22cbcb040749de362c3c01e171344cea31f
Yash Prolix <yash.prolix21@gmail.com>
2026-09-07 09:48:46 +0530

    fix button pointer.

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -76,7 +76,7 @@ export default function App() {
             </div>
             <button
               type="submit"
-              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg cursor-pointer"
             >
               Convert {from.toUpperCase()} to {to.toUpperCase()}
             </button>
