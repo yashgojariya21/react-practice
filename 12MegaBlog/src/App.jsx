eff7ef92563904e66f3a2d35986fa3d0704868bc
Yash Prolix <yash.prolix21@gmail.com>
2026-09-11 11:13:06 +0530

    setup MegaBlog project and setup AppWrite credentials.

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='align-center bg-yellow-500 p-3 text-3xl'>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
